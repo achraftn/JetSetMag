f4860df111d4edc10420a6f0c9ed769dc3dc284c
Achraf TOUNSI <achraf.tounsi@esprit.tn>
2014-02-20 15:38:17 +0100

    new Commit By Achraf

---
+++ src/jetsetmag/DAO/hhh.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jetsetmag.DAO;
+
+/**
+ *
+ * @author Achraf
+ */
+public class hhh {
+    String conflit;
+}
